7aaee9dfa07dcefe1cc3130597afa2d3ccb72d52
Murdock021 <diogomachadocmb@gmail.com>
2023-02-15 16:54:16 -0300

    (Criar conta sacar depósito e transferência)

---
+++ Prototype Bank/banco.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'src/Conta.php';
+require_once 'src/Titular.php';
+require_once 'src/CPF.php';
+
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Diogo Machadi');
+$primeiraConta = new Conta($vinicius);
+$primeiraConta->deposita(1000);
+$primeiraConta->saca(150); 
+
+echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
+echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
+echo $primeiraConta->recuperaSaldo() . PHP_EOL;
+
